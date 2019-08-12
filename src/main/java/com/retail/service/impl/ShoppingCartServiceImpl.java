a68245db50c13af72c33dd1ac903b30769348343
S912883 <anish.pavuluri@dnata.com>
2019-08-12 10:09:04 +0400

    removed unused imports

--- src/main/java/com/retail/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/retail/service/impl/ShoppingCartServiceImpl.java
@@ -16,7 +16,6 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
